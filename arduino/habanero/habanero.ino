193e08798116d5348729383c55156133580b815a
Ott, Mark <mark.ott@pearson.com>
2016-09-12 19:48:03 -0500

    habanero (arduino) now posting reading to database

--- arduino/habanero/habanero.ino
+++ arduino/habanero/habanero.ino
@@ -1,77 +1,106 @@
-#include <Console.h>
+
+#include <Process.h>
 
 const int ledPin = 13; // the pin that the LED is attached to
-int incomingByte;      // a variable to read incoming serial data into
 
 #include "DHT.h"
+
+#define ARDUINO_NAME "habanero"
+#define POST_READING_URL "http://192.168.1.8:8080/api/readings"
+
+#define DELAY 60000
 
 #define DHTPIN 2     // what digital pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
 DHT dht(DHTPIN, DHTTYPE);
-void setup() {
-  // initialize serial communication:
-  Bridge.begin();
-  Console.begin(); 
 
-  while (!Console){
-    ; // wait for Console port to connect.
-  }
-  Console.println("Connected to the Console!!!!");
+
+void setup() {
+
+  Bridge.begin();
+
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
-
   
   dht.begin();
 }
 
 void loop() {
-  // see if there's incoming serial data:
-//  if (Console.available() > 0) {
-//    // read the oldest byte in the serial buffer:
-//    incomingByte = Console.read();
-//    // if it's a capital H (ASCII 72), turn on the LED:
-//    if (incomingByte == 'H') {
-//      digitalWrite(ledPin, HIGH);
-//    } 
-//    // if it's an L (ASCII 76) turn off the LED:
-//    if (incomingByte == 'L') {
-//      digitalWrite(ledPin, LOW);
-//    }
-//  }
 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+    readDht();
 
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
+    delay(DELAY);
+
+}
+
+void ledOn() {
+    digitalWrite(ledPin, HIGH);
+}
+
+void ledOff() {
+    digitalWrite(ledPin, LOW);
+}
+
+void readDht() {
+    ledOn();
+
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //  float f = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) /* || isnan(f) */) {
+        Serial.println("Failed to read from DHT sensor!");
+        return;
+    }
+
+    postReading(h, "percent-humidity", "DHT22");
+
+    postReading(t, "degrees-celsius", "DHT22");
 
   // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
+//  float hif = dht.computeHeatIndex(f, h);
   // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+//  float hic = dht.computeHeatIndex(t, h, false);
 
-  Console.println("Humidity: ");
-  Console.println(h);
-  Console.println(" %\t");
-  Console.println("Temperature: ");
-  Console.println(t);
-  Console.println(" *C ");
-  Console.println(f);
-  Console.println(" *F\t");
-  Console.println("Heat index: ");
-  Console.println(hic);
-  Console.println(" *C ");
-  Console.println(hif);
-  Console.println(" *F");
+    ledOff();
+}
 
-  delay(10000);
+void postReading(float value, String uom, String component) {
+    // curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -H "Postman-Token: ba8099b2-fa56-74a7-0487-299f9f222c29"
+    // -d '{"value":"59", "device": "mark", "component": "door", "uom": {"name":"foot", "description":"Foot"}}' "http://gnomefly.local:8080/api/readings"
+
+    Process p;
+
+    String json = "{\"value\":\"" + String(value) + "\",\"device\":\"" + ARDUINO_NAME + "\",\"component\":\"" + component + "\",\"uom\":\"" + uom + "\"}";
+
+    Serial.print("Post Reading: ");
+    Serial.println(json);
+
+    p.begin("curl");
+
+    p.addParameter("-X");
+    p.addParameter("POST");
+    p.addParameter("-H");
+    p.addParameter("Content-Type: application/json");
+    p.addParameter("-H");
+    p.addParameter("Cache-Controle: no-cache");
+    p.addParameter("-d");
+    p.addParameter(json);
+    p.addParameter(POST_READING_URL);
+
+    p.run();
+
+    // A process output can be read with the stream methods
+    while (p.available()>0) {
+        char c = p.read();
+        Serial.print(c);
+    }
+    // Ensure the last bit of data is sent.
+    Serial.flush();
 }
